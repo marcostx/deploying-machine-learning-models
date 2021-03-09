a39fe3b889c7532753d5fe08d3b8ab4bd5dd8f36
Marcos <ecclesiedei@gmail.com>
2021-03-08 21:29:29 -0300

    test updating oath

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -9,7 +9,7 @@ pd.options.display.max_rows = 10
 pd.options.display.max_columns = 10
 
 
-PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+PACKAGE_ROOT = '/home/circleci/project/packages/regression_model/regression_model'
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 
